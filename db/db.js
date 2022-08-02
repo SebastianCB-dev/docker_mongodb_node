1c31c30779d6278110d24074b13abe468777b879
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-08-02 11:03:17 -0500

    Fix: Fixed connection between containers

--- db/db.js
+++ db/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-export async function databaseConnection() {
-  return await mongoose.connect('mongodb://sebastiancb:password@localhost:27017/pruebaDocker?authSource=admin');
+async function databaseConnection() {
+  return await mongoose.connect('mongodb://sebastiancb:password@mongodb:27017/pruebaDocker?authSource=admin');
 }
+
+module.exports = { databaseConnection }
